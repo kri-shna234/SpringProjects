5dbbe773bddcf75837903e996a12c78ce438057a
ramis <ramis@krishna>
2024-12-19 14:36:43 +0530

    bank application

---
+++ Dutch_Bank_Application/src/main/java/com/ibm/DutchBankApplication.java
@@ -0,0 +1,13 @@
+package com.ibm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutchBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DutchBankApplication.class, args);
+	}
+
+}
